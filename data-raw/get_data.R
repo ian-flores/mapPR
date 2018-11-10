011e55734185223e50b7742acd0dc99134245286
Ian Flores Siaca <iflores.siaca@gmail.com>
2018-11-10 15:12:06 -0800

    Uploading test

--- data-raw/get_data.R
+++ data-raw/get_data.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(geojsonio)
 library(jsonlite)
-library(sp)
-library(sf)
+library(usethis)
 
 barrios_url <- 'https://raw.githubusercontent.com/ian-flores/atlaspr/master/geotiles/barrios.json'
 barrios <- geojson_read(barrios_url, what = 'sp')
-barrios_points <- fortify(barrios, region = 'NAME') 
+barrios_points <- fortify(barrios, region = 'NAME')
+
+usethis::use_data(barrios, overwrite = TRUE)
 
 poverty_barrios_data <- fromJSON('https://raw.githubusercontent.com/ian-flores/atlaspr/master/data/barriosdata.json')[2:940, ]
 poverty_barrios_data %>%
@@ -14,3 +15,4 @@ poverty_barrios_data %>%
     mutate(geo_id = str_remove(geo_id, '(^[0-9][0-9][0-9][0-9][0-9][0-9][^A-Z][A-Z][^0-9][0-9][0-9][0-9][0-9].)')) ->
     poverty_barrios_data
 
+usethis::use_data(poverty_barrios_data, overwrite = TRUE)
